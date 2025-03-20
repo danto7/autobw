b01488bbe68cb09e62c8c8773c91b1dc54357b7f
Daniel Jensen <daniel@danto.cc>
2025-03-20 22:25:08 +0100

    add goreleaser

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
       let
         pkgs = import nixpkgs { inherit system; };
       in
+      with nixpkgs.legacyPackages.${system};
       {
         devShells = {
           default = pkgs.mkShell {
@@ -21,7 +22,26 @@
               # minikube
               # docker-client
               go
+              goreleaser
             ];
+          };
+        };
+        defaultPackage = buildGoModule rec {
+          pname = "autobw";
+          version = "0.3.4";
+
+          src = builtins.path {
+            path = ./.;
+            name = "${pname}-src";
+          };
+
+          vendorHash = "sha256-kUZ2CxKfx/QeKXxix24Ld9lK50MEENuH5HS9gWY8zZo=";
+
+          meta = {
+            description = "Command line tool to manage Bitwarden cli sessions in the keychain";
+            homepage = "https://github.com/danto7/autobw";
+            license = lib.licenses.mit;
+            maintainers = with lib.maintainers; [ danto7 ];
           };
         };
       }
